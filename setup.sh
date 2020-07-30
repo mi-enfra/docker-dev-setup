6c58c1d82a05cc04519ff0bfb75bee84c49bac10
mi-enfra <54035644+mi-enfra@users.noreply.github.com>
2020-07-31 01:23:03 +0800

    📝 Install Docker and Docker Compose

--- setup.sh
+++ setup.sh
@@ -2,3 +2,15 @@
 
 sudo apt update -y
 sudo apt upgrade -y
+
+# Source: https://www.digitalocean.com/community/tutorials/how-to-install-and-use-docker-on-ubuntu-18-04
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo apt update
+sudo apt install docker-ce
+sudo usermod -aG docker ${USER}
+
+# Source: https://www.digitalocean.com/community/tutorials/how-to-install-docker-compose-on-ubuntu-18-04
+sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
