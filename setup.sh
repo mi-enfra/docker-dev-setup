3f50c1bf669a3075e72a030bb06fd387cb0c7b9d
Marvin Isaac <misaac@pushnami.com>
2020-08-01 05:13:03 +0800

    :alembic: Install node and npm globally

--- setup.sh
+++ setup.sh
@@ -20,3 +20,6 @@ curl -sS https://getcomposer.org/installer -o composer-setup.php
 HASH=$(curl https://composer.github.io/installer.sig)
 php -r "if (hash_file('SHA384', 'composer-setup.php') === '$HASH') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+nvm install node
