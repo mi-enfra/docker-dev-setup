09c8acd8ce1340ab0862872c93cb12aecdce3c75
Marvin Isaac <misaac@pushnami.com>
2020-08-01 06:09:22 +0800

    :alembic: Enable immediate usage of node and npm

--- setup.sh
+++ setup.sh
@@ -26,3 +26,5 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 nvm install node
+# Use node and npm immediately
+source ~/.bashrc
