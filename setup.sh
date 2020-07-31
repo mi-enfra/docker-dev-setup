26b6ed404501d2361fc62388a3782677d38160ca
Marvin Isaac <misaac@pushnami.com>
2020-08-01 03:33:47 +0800

    :alembic: Default to accepting command

--- setup.sh
+++ setup.sh
@@ -4,11 +4,11 @@ sudo apt update -y
 sudo apt upgrade -y
 
 # Source: https://www.digitalocean.com/community/tutorials/how-to-install-and-use-docker-on-ubuntu-18-04
-sudo apt install apt-transport-https ca-certificates curl software-properties-common
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
-sudo apt update
-sudo apt install docker-ce
+sudo apt update -y
+sudo apt install docker-ce -y
 sudo usermod -aG docker ${USER}
 
 # Source: https://www.digitalocean.com/community/tutorials/how-to-install-docker-compose-on-ubuntu-18-04
